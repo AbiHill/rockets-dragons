95cc607fc5b5d445bbc6f599f9da7e03e512b05c
Abi Hill <abihill8@gmail.com>
2020-10-03 16:14:36 +0100

    adding in the API call for both endpoints for dragons and rockets

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,6 +7,24 @@ class Home extends React.Component {
     this.state = {}
   }
   
+  componentDidMount() {
+    this.callContentApi('https://api.spacexdata.com/v3/rockets', 'rockets');
+    this.callContentApi('https://api.spacexdata.com/v3/dragons', 'dragons');
+  }
+  
+  callContentApi = async (endpoint, type) => {
+    try {
+      const response = await fetch(endpoint);
+      if (response.ok) {
+        const jsonResp = await response.json();
+        this.setState({ [type] : jsonResp});
+      }
+    }
+    catch(err) {
+      console.log(err)
+    }
+  }
+  
   render() {
     return (
       <>
