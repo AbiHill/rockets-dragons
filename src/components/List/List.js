0b19214edf517c31b07ed50565433e450b1870e4
Abi Hill <abihill8@gmail.com>
2020-10-03 16:06:02 +0100

    project directories set up and start of list component

---
+++ src/components/List/List.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const List = () => {
+  return(
+    <div>
+      <p>Hello, I am a list</p>
+    </div>
+  )
+}
+
+export default List;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import List from '../../components/List/List';
+
+class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+  
+  render() {
+    return (
+      <>
+        <List />
+      </>
+    )
+  }  
+}
+
+export default Home;
