9efc45b55993b3b1fefedb7eb9da1d0eb4eef202
Abi Hill <abihill8@gmail.com>
2020-10-03 21:24:02 +0100

    including .env file for eslint error and also included some detail on the panel view

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,16 +3,17 @@ import React from 'react';
 import { ListWrapper, ListItem, ListTitle, ListImg, ListButton } from './styles';
 
 const List = (props) => (
-    <ListWrapper>
-      {props.data &&
-        props.data.map((item) => (
-          <ListItem onClick={props.onClick} key={item.id} id={item.id}>
-            <ListTitle>{item.name || item.rocket_name}</ListTitle>
-            <ListImg src={item.flickr_images[0]} alt={item.name || item.rocket_name} />
-            <ListButton>More info</ListButton>
-          </ListItem>
-        ))}
-    </ListWrapper>
-  );
+  <ListWrapper>
+    { props.data && (
+      props.data.map((item) => (
+        <ListItem onClick={props.onClick} key={item.id} id={item.id}>
+          <ListTitle>{item.name || item.rocket_name}</ListTitle>
+          <ListImg src={item.flickr_images[0]} alt={item.name || item.rocket_name} />
+          <ListButton>More info</ListButton>
+        </ListItem>))
+      )
+}
+  </ListWrapper>
+);
 
 export default List;
--- src/components/Panel/Panel.js
+++ src/components/Panel/Panel.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
-const Panel = (props) => <div>{props.data && <p>{props.data.active} hi I'm a dragon</p>}</div>;
+const Panel = (props) => (
+  <div>
+    {props.data && (
+      <div>
+        <h2>{props.data.name || props.data.rocket_name}</h2>
+        <p>{props.data.description}</p>
+        <img src={props.data.flickr_images} alt='alt text' />
+      </div>
+    )}
+  </div>
+);
 
 export default Panel;
